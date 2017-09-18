0fad2975366895072226d2d065ac6dd9f30deab5
sandeepforever007 <snalla@uncc.edu>
2017-09-17 21:53:20 -0500

    Commit 1

--- DelegateDemp/DelegateDemo1/Program.cs
+++ DelegateDemp/DelegateDemo1/Program.cs
@@ -1,28 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
+
+delegate bool IsPromotable(Employee emp1);
+delegate bool IsPromotableBySalary(Employee emp2);
+delegate void MulticastDelegate();
+
 public class WithDelClassEx
 {
 
-	public static void Main()
+	public static void Main(string[] Args)
 	{
 
 		List<Employee> emplist = new List<Employee>
 		{
-			new Employee() { Id = 1, Name = "Sandeep", Experience = 8 , Salary = 20 },
-			new Employee() { Id = 2, Name = "Rohith", Experience = 6, Salary = 10 },
-			new Employee() { Id = 3, Name = "Omkar", Experience = 10 , Salary = 5},
-			new Employee() { Id = 4, Name = "Vishnu", Experience = 9 , Salary = 6}
+			new Employee() { Id = 1, Name = "Sandeep", Experience = 8 , salary = 20 },
+			new Employee() { Id = 2, Name = "Rohith", Experience = 6, salary = 10 },
+			new Employee() { Id = 3, Name = "Omkar", Experience = 10, salary = 15},
+			new Employee() { Id = 4, Name = "Vishnu", Experience = 7 , salary = 6}
 		};
 
 		IsPromotable isPromotable = new IsPromotable(EligibleEmp);
-		isPromotable += EligibleEmpSalary;//Multi Cast Delegate
-		//IsPromotable promoteSal = new IsPromotable(EligibleEmpSalary);
+		//isPromotable += EligibleEmpSalary;
+		IsPromotableBySalary promoteSal = new IsPromotableBySalary(EligibleEmpSalary);
 		//isPromotable += promoteSal;
-		Employee.PromoteEmployee(emplist, isPromotable);
+		Employee.PromoteEmployee(emplist, isPromotable, promoteSal);
 		//Employee.PromoteEmployee(emplist, emp => emp.Experience >= 8);
+
+		//Multicast Delegate
+		Console.WriteLine("Multicast Delegate");
+		MulticastDelegate multi = MethodOne;
+		multi += MethodTwo;
+		multi();
 		Console.ReadKey();
 	}
+
+
+	public static void MethodOne()
+	{
+		Console.WriteLine("MethodOne");
+	}
+
+	public static void MethodTwo()
+	{
+		Console.WriteLine("MethodTwo");
+	}
+
+
 
 	static bool EligibleEmp(Employee ee)
 	{
@@ -38,7 +63,7 @@ public class WithDelClassEx
 
 	static bool EligibleEmpSalary(Employee e2)
 	{
-		if (e2.Salary >= 10)
+		if (e2.salary >= 10)
 		{
 			return true;
 		}
@@ -50,7 +75,6 @@ public class WithDelClassEx
 
 }
 
-delegate bool IsPromotable(Employee emp1);
 
 
 class Employee
@@ -59,15 +83,18 @@ class Employee
 	public int Id { set; get; }
 	public string Name { set; get; }
 	public int Experience { set; get; }
-	public int Salary { get; set; }
+	public int salary { get; set; }
 
-	public static void PromoteEmployee(List<Employee> emp, IsPromotable IsEligibleToPromote)
+	public static void PromoteEmployee(List<Employee> emp, IsPromotable IsEligibleToPromote, IsPromotableBySalary IsEligibleBySalary)
 	{
 		foreach (Employee employee in emp)
 		{
 			if (IsEligibleToPromote(employee))
 			{
-				Console.WriteLine("Employee" + " " + employee.Name + " " + "Promoted");
+				if (IsEligibleToPromote(employee))
+				{
+					Console.WriteLine("Employee" + " " + employee.Name + " " + "Promoted");
+				}
 			}
 		}
 	}
